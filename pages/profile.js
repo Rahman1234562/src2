b9e6f318d40b4af8b1c44b58747fc7d49fa6e5fa
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-12-06 23:08:26 -0800

    rewrite code

--- pages/profile.js
+++ pages/profile.js
@@ -4,10 +4,7 @@ import { getSession } from "next-auth/react"
 export default function Profile  () {
     return(
         <>
-        <div className="flex justify-center mt-6 font-semibold text-xl">My Profile</div>
-        <div className="h-96 bg-black w-2/4 flex justify-center mt-4 pr-10">
-            <div className="h-52 bg-white w-96 flex justify-start mt-2 relative right-64"></div>
-        </div>
+          <div>this is a div</div>
         </>
     )
 }
